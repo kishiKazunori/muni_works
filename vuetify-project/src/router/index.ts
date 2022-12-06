f6fae4b376e34482b3c0bd6b4a7e63409f5f0e63
kazunori kishi <contact.kishi@gmail.com>
2022-12-06 07:16:03 +0000

    Add about path

--- vuetify-project/src/router/index.ts
+++ vuetify-project/src/router/index.ts
@@ -1,27 +1,32 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 
 const routes: Array<RouteRecordRaw> = [
-    {
-        path: '/',
-        name: 'topPage',
-        component: () => import('../views/TopPage.vue'),
-    },
-      {
-        path: '/isiPage',
-        name: 'isiPage',
-        component: () => import('../views/ArtBookIsiPage.vue'),
-      },
-      {
-        path: '/uPage',
-        name: 'uPage',
-        component: () => import('../views/ArtBookUuPage.vue'),
-      },
+  {
+    path: '/',
+    name: 'topPage',
+    component: () => import('../views/TopPage.vue'),
+  },
+  {
+    path: '/isiPage',
+    name: 'isiPage',
+    component: () => import('../views/ArtBookIsiPage.vue'),
+  },
+  {
+    path: '/uPage',
+    name: 'uPage',
+    component: () => import('../views/ArtBookUuPage.vue'),
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/MuniAbout.vue'),
+  },
 ];
 
 const router = createRouter({
-    // Viteの環境変数でimport.meta.env.BASE_URL = vite.config.tsのbase
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes,
+  // Viteの環境変数でimport.meta.env.BASE_URL = vite.config.tsのbase
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
 });
 
 export default router;
