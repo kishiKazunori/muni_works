4b8150792718b0ab1c94bc8afefe09a041185427
kazunori kishi <contact.kishi@gmail.com>
2022-12-07 04:59:49 +0000

    Change setup code

--- vuetify-project/src/components/MenuBar.vue
+++ vuetify-project/src/components/MenuBar.vue
@@ -1,44 +1,41 @@
 <template>
-    <v-tabs v-model="tab" color="basil" grow>
-        <v-tab :to="item.value" class="menu-tab" v-for="item in items" :key="item.name" :value="item">
-            {{ item.name }}
-        </v-tab>
-    </v-tabs>
+  <v-tabs v-model="selectedTab" grow>
+    <v-tab
+      :to="item.value"
+      class="menu-tab"
+      v-for="item in tabLinkItems"
+      :key="item.name"
+      :value="item"
+    >
+      {{ item.name }}
+    </v-tab>
+  </v-tabs>
 </template>
- 
-<script lang="ts">
-import { defineComponent, ref } from "vue";
 
-export default defineComponent({
-    name: "MenuBar",
-    setup() {
-        const tab = ref("いし。");
-        const items = ref([
-                {
-                    value: "/isiPage",
-                    name: "いし。"
-                },{
-                    value: "/uPage",
-                    name: "u"
-                },
-                {
-                    value: "/aboutPage",
-                    name: "about"
-                },
-            ]);
-        return {
-            tab,
-            items
-        };
-    }
-})
+<script setup lang="ts">
+import { reactive, ref } from "vue";
+
+const selectedTab = ref("いし。");
+const tabLinkItems: any = reactive([
+  {
+    value: "/isiPage",
+    name: "いし。",
+  },
+  {
+    value: "/uPage",
+    name: "u",
+  },
+  {
+    value: "/aboutPage",
+    name: "about",
+  },
+]);
 </script>
- 
- 
-<style  lang="scss" scoped>
+
+<style lang="scss" scoped>
 .menu-tab {
-    font-size: 2em;
-    font-family: 'Zen Old Mincho', sans-serif;
-    text-transform: none;
+  font-size: 2em;
+  font-family: "Zen Old Mincho", sans-serif;
+  text-transform: none;
 }
 </style>
