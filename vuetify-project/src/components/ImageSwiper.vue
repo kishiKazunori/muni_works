9da7e28be19cd6e712bd89d8abcae0fe17f25946
kazunori kishi <contact.kishi@gmail.com>
2022-12-07 04:54:26 +0000

    Add mobile image swiper

--- vuetify-project/src/components/ImageSwiper.vue
+++ vuetify-project/src/components/ImageSwiper.vue
@@ -1,36 +1,61 @@
 <template>
+  <v-icon class="image-close-icon">mdi-close-circle-outline</v-icon>
   <swiper
     :slides-per-view="1"
-    :space-between="50"
+    :space-between="15"
+    :loop="true"
+    :initialSlide="selectedImageNumber"
+    :pagination="myPaginationStyle"
+    :modules="modules"
     @swiper="onSwiper"
     @slideChange="onSlideChange"
+    style="color: #fff"
   >
-    <swiper-slide>Slide 1</swiper-slide>
-    <swiper-slide>Slide 2</swiper-slide>
-    <swiper-slide>Slide 3</swiper-slide>
+    <swiper-slide v-for="(item, i) in imageItems" :key="i">
+      <v-img :src="item.imageSrc"></v-img>
+    </swiper-slide>
   </swiper>
 </template>
 
-<script lang="ts">
-import { computed, defineComponent, onMounted, ref } from "vue";
+<script setup lang="ts">
 import { Swiper, SwiperSlide } from "swiper/vue";
+import { Pagination } from "swiper";
 import "swiper/css";
-export default defineComponent({
-  components: {
-    Swiper,
-    SwiperSlide,
-  },
-  setup() {
-    const onSwiper = (swiper) => {
-      console.log(swiper);
-    };
-    const onSlideChange = () => {
-      console.log("slide change");
-    };
-    return {
-      onSwiper,
-      onSlideChange,
-    };
-  },
-});
+// import "swiper/css/pagination";
+
+// Props
+const props = defineProps<{
+  imageItems: any;
+  selectedImageNumber :number;
+}>();
+
+// data
+const myPaginationStyle = {
+  clickable: true,
+  type: "fraction",
+};
+const modules = [Pagination];
+
+// methods
+const onSwiper = (swiper: any) => {
+  console.log(swiper);
+  console.log(props.imageItems);
+};
+const onSlideChange = () => {
+  console.log("slide change");
+};
 </script>
+
+<style lang="scss">
+.swiper-pagination {
+display: flex;
+justify-content: center;
+justify-items: center;
+}
+.image-close-icon{
+  position:absolute;
+  right:0px;
+  top:-30px;
+  z-index: 99;
+}
+</style>
