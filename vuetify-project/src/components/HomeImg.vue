506e01e491b95ada45cbe8dd90a49f0708a12092
kazunori kishi <contact.kishi@gmail.com>
2022-12-07 04:53:46 +0000

    Change class name

--- vuetify-project/src/components/HomeImg.vue
+++ vuetify-project/src/components/HomeImg.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-container fluid ma-0 pa-0 fill-height class="home-hero__content">
+    <v-container fluid fill-height class="home-image">
     </v-container>
 </template>
 
@@ -9,14 +9,11 @@
 
 
 <style  lang="scss" scoped>
-.home-hero__content {
+.home-image {
     background: url("/u-ehon-image/home-main-img.png");
     background-size: cover;
     background-position: center center;
     width: 100vw;
     height: 80vh;
-    &-text {
-        font-size: 100px;
-    }
 }
 </style>
