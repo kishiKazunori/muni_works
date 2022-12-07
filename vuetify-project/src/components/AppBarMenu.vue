8a6c2ad9d003a138178fe24e9b34b3e1233cfac8
kazunori kishi <contact.kishi@gmail.com>
2022-12-07 04:50:49 +0000

    Change setup

--- vuetify-project/src/components/AppBarMenu.vue
+++ vuetify-project/src/components/AppBarMenu.vue
@@ -12,41 +12,36 @@
   </v-app-bar>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref } from "vue";
-export default defineComponent({
-  name: "AppBarMenu",
-  setup() {
-    const contents = ref([
-      { name: "TOP", value: "/" },
-      { name: "いし。", value: "/isiPage" },
-      { name: "u", value: "/uPage" },
-      { name: "about", value: "/about" },
-    ]);
-    return {
-      contents,
-    };
-  },
-});
+<script setup lang="ts">
+import { reactive } from "vue";
+
+const linkContents: any = reactive([
+  { name: "TOP", value: "/" },
+  { name: "いし。", value: "/isiPage" },
+  { name: "u", value: "/uPage" },
+  { name: "about", value: "/about" },
+]);
 </script>
 
 <style lang="scss" scoped>
-.app-bar-button {
-  font-size: 1.5em;
+.v-toolbar-title__placeholder {
+  overflow: initial !important;
+}
+.app-bar-links {
+  font-size: 1em;
   font-family: "Zen Old Mincho", sans-serif;
 }
 
-.app-bar-title-button {
-  font-size: 2em;
-}
-
 @media screen and (max-width: 767px) {
-  .app-bar-button {
-    font-size: 0.7em;
+  .app-bar-title {
+    font-size: 0.5em;
+    width: 50vw !important;
   }
-
-  .app-bar-title-button {
-    font-size: 0.7em;
+  .app-bar-links {
+    font-size: 1px;
+    margin: 0 !important;
+    padding: 0;
+    font-family: "Zen Old Mincho", sans-serif;
   }
 }
 </style>
