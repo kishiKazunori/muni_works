290101ceb61e0b30145b45517646b2410a99ae98
kazunori kishi <contact.kishi@gmail.com>
2022-12-10 12:16:41 +0000

    Change format

--- vuetify-project/src/components/LogoText.vue
+++ vuetify-project/src/components/LogoText.vue
@@ -1,17 +1,16 @@
 <template>
-   <div class="text-h1 font-weight-bold logo-text">muni works</div>
+  <div class="text-h1 font-weight-bold logo-text">muni works</div>
 </template>
 
 <script setup lang="ts">
-    //
+//
 </script>
 
-
-<style  lang="scss" scoped>
-.logo-text{
-    text-shadow:1px 1px 0 #FFF, -1px -1px 0 #FFF,
-              -1px 1px 0 #FFF, 1px -1px 0 #FFF,
-              0px 1px 0 #FFF,  0-1px 0 #FFF,
-              -1px 0 0 #FFF, 1px 0 0 #FFF;
+<style lang="scss" scoped>
+.logo-text {
+  text-shadow: 1px 1px 0 #fff, -1px -1px 0 #fff, -1px 1px 0 #fff,
+    1px -1px 0 #fff, 0px 1px 0 #fff, 0-1px 0 #fff, -1px 0 0 #fff, 1px 0 0 #fff;
+  cursor: pointer;
 }
+
 </style>
