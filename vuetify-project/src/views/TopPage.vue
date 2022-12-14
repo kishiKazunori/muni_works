a0088643c1b68aca6a18e2fcae28e7a25a750317
kazunori kishi <contact.kishi@gmail.com>
2022-12-14 12:27:00 +0000

    Update top transition animation

--- vuetify-project/src/views/TopPage.vue
+++ vuetify-project/src/views/TopPage.vue
@@ -1,24 +1,35 @@
 <template>
-    <v-app class="container">
-      <v-main>
+  <v-app class="container">
+    <v-main>
+      <div class="top-page-fade-animation">
         <HomeImg />
-        <LogoText class="logo-text-position"/>
+        <LogoText transition="scroll-x-transition" class="logo-text-position" />
         <TopPageMenuBar />
-      </v-main>
-    </v-app>
-  </template>
-  
-  <script setup lang="ts">
-  import HomeImg from '../components/HomeImg.vue';
-  import LogoText from '../components/LogoText.vue';
-  import TopPageMenuBar from '../components/TopPageMenuBar.vue';
-  </script>
-  
-  <style  lang="scss" >
-  .logo-text-position{
-    position: absolute;
-      top: 30vw;
-      left: 10vh;
-  }
-  
-  </style>
+      </div>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import HomeImg from "../components/HomeImg.vue";
+import LogoText from "../components/LogoText.vue";
+import TopPageMenuBar from "../components/TopPageMenuBar.vue";
+import { onMounted } from "vue";
+import anime from "animejs";
+onMounted(() => {
+  anime({
+    targets: ".top-page-fade-animation",
+    opacity: [0, 1],
+    easing: "easeInOutCubic", // 加減速の種類
+    duration: 900, // ミリ秒指定
+  });
+});
+</script>
+
+<style lang="scss">
+.logo-text-position {
+  position: absolute;
+  top: 30vw;
+  left: 10vh;
+}
+</style>
