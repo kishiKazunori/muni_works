24ef088d189e541735e775eac93e763d4bd4df21
kazunori kishi <contact.kishi@gmail.com>
2022-12-19 10:57:56 +0000

    Update design

--- vuetify-project/src/components/HomeImg.vue
+++ vuetify-project/src/components/HomeImg.vue
@@ -15,17 +15,17 @@
     background-position: center center;
     animation-duration: 1s;
     width: 100vw;
-    height: 70vh;
+    height: 75vh;
     animation-name: animation;
 }
 
 @keyframes animation{
   /*1秒かけてwidth: 50%;→width: 100%;になる動きを記述*/
   0%{
-    height: 50vh;
+    height: 55vh;
   }
   100%{
-    height: 70vh;
+    height: 75vh;
   }
 }
 </style>
